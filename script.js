e0d74bda529149c292311534159b486e88314a27
Koop Reynders <k.g.reynders@hva.nl>
2021-11-25 15:35:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 /*
 Micro interactie met javascript: 
-0: queryselector om de button en de header te kunnen gebruiken in js
-1: click op de button
-2: met classlist toggle de class "show-search" op de header zetten
+1: queryselector om de button en de header te kunnen gebruiken in js
+2: click op de button
+3: met classlist toggle de class "show-search" op de header zetten
 
 
 artikel: Hey designers, if you only know one thing about JavaScript, this is what I would recommend 
@@ -11,17 +11,16 @@ https://css-tricks.com/video-screencasts/150-hey-designers-know-one-thing-javasc
 
 */
 
-//0: queryselector om de button en de header te kunnen gebruiken in js
+//1: queryselector om de button en de header te kunnen gebruiken in js
 var deButton = document.querySelector("header > button")
 var deHeader = document.querySelector("header")
 
-//1: click op de button om een functie uit te voeren
+//2: click op de button om een functie uit te voeren
 deButton.addEventListener("click",doiets);
 
-//2: met classlist toggle de class "show-search" op de header zetten
+//3: met classlist toggle de class "show-search" op de header zetten
 function doiets(){
     deHeader.classList.toggle("show-search");
-
 
     // if(deHeader.classList.contains("show-search")){
     //     deHeader.classList.remove("show-search");
